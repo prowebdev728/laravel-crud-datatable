491d1b54d7a0d8900ab91abca293e4635c45099b
dragon-PC\dragon <proweb.developer2016@yandex.com>
2017-03-06 15:08:31 +0100

    datatable1Model variable arrange

--- app/Datatable1Model.php
+++ app/Datatable1Model.php
@@ -8,9 +8,10 @@ class Datatable1Model extends Model
 {
     //set table
     protected $table = 'users';
-    
+
     //set primary key
     protected $primaryKey = 'id';
+    
     //when not use table field - updated_at and created_at
     public $timestamps = false;
 }
